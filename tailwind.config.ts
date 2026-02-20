4712d17fca44c841e998ce6be4b29043b594fb07
Igor Pinheiro <igordf@gmail.com>
2026-02-20 15:04:24 -0300

    feat: adicionar configuração do Tailwind CSS com safelist de classes

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'tailwindcss'
+
+export default <Config>{
+  safelist: [
+    'bg-green-500',
+    'text-white',
+    'font-bold',
+    'bg-green-200',
+    'text-green-900',
+    'bg-blue-100',
+    'text-blue-800',
+    'bg-yellow-100',
+    'text-yellow-800',
+    'bg-orange-100',
+    'text-orange-800',
+    'bg-red-100',
+    'text-red-800',
+  ],
+}
